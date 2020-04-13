af8655d8cfc40c545b4922c79259d7e163214f3f
chloebates4 <chloe.bates4@gmail.com>
2020-04-13 10:51:11 -0700

    almost works

--- src/appserver/satellite/Satellite.java
+++ src/appserver/satellite/Satellite.java
@@ -8,6 +8,7 @@ import static appserver.comm.MessageTypes.JOB_REQUEST;
 import static appserver.comm.MessageTypes.REGISTER_SATELLITE;
 import appserver.job.Tool;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
@@ -20,52 +21,40 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import utils.PropertyHandler;
 
-/**
- * Class [Satellite] Instances of this class represent computing 
- * nodes that execute jobs by calling the callback method of tool a 
- * implementation, loading the tool's code dynamically over a network
- * or locally from the cache, if a tool got executed before.
- * 
- * FROM ASSIGNMENT 7 DESCRIPTION:
- * 
- * Satellite servers are actually doing the work of the application 
- * server - the latter only being a way to access satellites. 
- * 
- * Thus satellites take on Jobs, get the right tool to do the job, 
- * process the job and get the results back to whoever requested the job to be done.
- * 
- * Application server adds value by: 
+/** ASSIGNMENT 7: APPLICATION SERVER - SATELLITE SERVER
+	 * 	@author Chloe Bates 	
+	 * 	@author Sam Guilb	
+	 * 	@author Colton Spector
+ * -----------------------------------------------------------------------------------------
+ * Class [Satellite] Instances of this class represent computing nodes that execute jobs by
+ * calling the callback method of tool a implementation, loading the tool's code dynamically 
+ * over a network or locally from the cache, if a tool got executed before.
+ * -----------------------------------------------------------------------------------------
+ *  Satellite servers are actually doing the work of the application server - the latter only
+ *  being a way to access satellites. Thus satellites: 
+ * 		--> take on Jobs, 
+ * 		--> get the right tool to do the job, 
+ * 		--> process the job & get the results back to whoever requested the job to be done.
+ *  Application server adds value by: 
  * 		(a) letting the registering happen for satellite servers
  * 		(b) providing load-balancing based in some policy. 
- * 
- * Satellite servers can be run on their own. 
- * 
- * ** Concentrate on the satellite server implementation, without getting 
- * side-tracked by the details and complexity of the overall application server structure.
- * 
- * 
- * TODO: Before you start the whole application, you will first need to MOVE the PlusOne tool consisting of two classes 
- * in package appserver.job.impl.plusone to the appropriate directory (mimicking the package structure) in the docRoot. 
- * Make sure to MOVE, otherwise the classes are found prematurely in the class path and won't be loaded over the network through the web server.
- * 
- * Then start the webserver, a satellite server and finally the client - in this order. Pay attention to the following:
- * All three programs need to get access to the correct properties files. The way how I set up the directory structure makes sure that happens. Check that the directories config (which contains the properties files) and also the docRoot are in your NetBeans project at the very same spot as they are in the package that you received.
- * Now start all three programs, each one of them in their own terminal window (don't start them from the IDE!!!). In each of the three terminal windows, go into the directory where the beginning of the class path for all compiled classes is, which is in build/classes, then start the program you want to run. The command lines, for your convenience, are as follows:
- * java web/SimpleWebServer ../../config/WebServer.properties
- * java appserver.satellite.Satellite ../../config/Satellite.Earth.properties ../../config/WebServer.properties ../../config//Server.properties
- * java appserver.job.impl.plusone.PlusOneClient ../../config/Satellite.Earth.properties
+ * 	Satellite servers can be run on their own. 
+ * -----------------------------------------------------------------------------------------
+ * --> Concentrate on satellite server implementation, w/out getting side-tracked 
+ * by the details & complexity of the overall application server structure.
  */
 public class Satellite extends Thread {
 
     private ConnectivityInfo satelliteInfo = new ConnectivityInfo();
     private ConnectivityInfo serverInfo = new ConnectivityInfo();
     private HTTPClassLoader classLoader = null;
-    private Hashtable<String, Tool> toolsCache = null;
+    private HashMap<String, Tool> toolsCache = null;
 
     /**
+     * Satellite () 
+     * -----------------------------------------------------------------------------------------
      * Note: just pass in, and read the application server properties file, 
      * as you will not want to change the satellite server's constructor
-     * 
      * @param satellitePropertiesFile
      * @param classLoaderPropertiesFile
      * @param serverPropertiesFile
@@ -73,88 +62,140 @@ public class Satellite extends Thread {
     public Satellite(String satellitePropertiesFile, 
     				 String classLoaderPropertiesFile, 
     				 String serverPropertiesFile) {
+    	// satellite's configuration and properties
+    	PropertyHandler satConfig; 
+    	String satName; int satPort; 
     	
-    	// satellite's configuration and properties from file passed in
-    	PropertyHandler satConfig; String satName; int satPort; 
+    	// server's configuration and properties 
+    	PropertyHandler serverConfig; 
+    	String serverHost; int serverPort; 
     	
-    	// server's configuration and properties from file passed in
-    	PropertyHandler serverConfig; String serverName; int serverPort; 
-    	
-    	// class loader's configuration and properties from file passed in
-    	PropertyHandler classConfig; String className; int classPort; 
+    	// class loader's configuration and properties 
+    	PropertyHandler classConfig; 
+    	String classHost; int classPort; 
     	
     	
-        // read this SATELLITE'S properties and populate satelliteInfo object,
-        // which later on will be sent to the server
+        // read satellitePropertiesFile & populate satelliteInfo which later on will be sent to the server
+        // ---------------------------------------------------------------
         try {
-        	 // read satellite's properties 
+        	 // read properties 
         	 satConfig = new PropertyHandler(serverPropertiesFile); 
-        	 
         	 // populate satelliteInfo 
-        	 satName = satConfig.getProperty("NAME");
-        	 satPort = Integer.parseInt(satConfig.getProperty("PORT")); //convert to type int
+        	 satelliteInfo.setName(satConfig.getProperty("NAME"));
+        	 satelliteInfo.setPort(Integer.parseInt(satConfig.getProperty("PORT")));
         	 
-        } catch (Exception e) {
+        	 // Q: set Host? 
+        	 
+        } catch (FileNotFoundException e) {
         	// no config file
-        	e.printStackTrace(); 
+        	System.err.println("[Satellite.constructor] CAUGHT -- FileNotFound Exception\n"+e); 
+        	System.exit(1);
+        } catch (IOException e) {
+        	// no config file
+        	System.err.println("[Satellite.constructor] CAUGHT -- IO Exception\n"+e); 
         	System.exit(1);
         }
         
-        // read properties of the APPLICATION SERVER and populate serverInfo object
-        // other than satellites, the as doesn't have a human-readable name, so leave it out
+        // read serverPropertiesFile & populate serverInfo object 
+        // ---------------------------------------------------------------
         try {
-        	 // read server's properties 
+        	 // read properties 
         	 serverConfig = new PropertyHandler(serverPropertiesFile);
         	 // populate serverInfo 
-        	 serverName = serverConfig.getProperty("NAME");
-        	 serverPort = Integer.parseInt(serverConfig.getProperty("PORT")); //convert to type int
-       	 
-        } catch (Exception e) {
+        	 serverInfo.setHost(serverConfig.getProperty("HOST"));
+        	 serverInfo.setPort(Integer.parseInt(serverConfig.getProperty("PORT"))); 
+
+        }catch (FileNotFoundException e) {
         	// no config file
-        	e.printStackTrace(); 
+        	System.err.println("[Satellite.constructor] CAUGHT -- FileNotFound Exception\n"+e); 
+        	System.exit(1);
+        } catch (IOException e) {
+        	// no config file
+        	System.err.println("[Satellite.constructor] CAUGHT -- IO Exception\n"+e); 
         	System.exit(1);
         }
         
-        // read properties of the code server and create class loader
-        // -------------------
+        // read properties of classLoaderPropertiesFile & create classLoader
+        // ---------------------------------------------------------------
         try {
-	       	 // read classLoader's properties 
+	       	 // read properties 
 	       	 classConfig = new PropertyHandler(classLoaderPropertiesFile);
-	       	 // populate serverInfo 
-	       	 className = classConfig.getProperty("NAME");
+	       	 classHost = classConfig.getProperty("HOST");
 	       	 classPort = Integer.parseInt(classConfig.getProperty("PORT")); //convert to type int
-	      	 
+	       	 // populate class loader
+	       	 classLoader = new HTTPClassLoader(classHost, classPort); 
+	       	 
+       } catch (FileNotFoundException e) {
+       		System.err.println("[Satellite.constructor] CAUGHT -- FileNotFound Exception\n"+e); 
+       		System.exit(1);
+       } catch (IOException e) {
+       		System.err.println("[Satellite.constructor] CAUGHT -- IO Exception\n"+e); 
+       		System.exit(1);
        } catch (Exception e) {
-	       	// no config file
-	       	e.printStackTrace(); 
+    	    System.err.println("[Satellite.constructor]"+e); 
 	       	System.exit(1);
        }
         
-        // create tools cache
-        // -------------------
+        // is the classLoader working properly? 
+        if( classLoader == null )
+        {
+            System.err.println("[Satellite.constructor] Unable to create HTTPClassLoader....");
+            System.exit(1);
+        }
+        
+        
+        // CREATE TOOLS CACHE
+        // ---------------------------------------------------------------
+        // note: param types are set in it's initialization above
         toolsCache = new HashMap<>(); 
         
     }
 
+    /**
+     * run () 
+     * -----------------------------------------------------------------------------------------
+     */
     @Override
     public void run() {
 
         // IGNORE: register this satellite with the SatelliteManager on the server
         // ---------------------------------------------------------------
-        // ...
         
         
         // create server socket
-        // ---------------------------------------------------------------
-        // ...
-        
-        
-        // start taking job requests in a server loop
-        // ---------------------------------------------------------------
-        // ...
+        // ---------------------------------------------------------------        
+    	try {
+    		ServerSocket serverSocket = new ServerSocket(satelliteInfo.getPort()); 
+    		System.out.println("[Satellite.run] Server socket created."); 
+    		
+    		// start taking job requests in a server loop
+            // ---------------------------------------------------------------
+    		while (true) {
+    			System.out.println("[Satellite.run] PORT #" + satelliteInfo.getPort()  
+						   		+ " waiting for a connection..." );
+    			
+    			Socket socket= serverSocket.accept(); // acceptance of new connections
+    			System.out.println("[Satellite.run] New connection was created."); 
+    			
+    			// thread for job to be completed
+    			(new SatelliteThread (socket, this)).start(); 
+    		}		
+    	
+    	} catch (Exception e) {
+    		System.err.println("[Satellite.run] Unable to connect.   " + e); 
+    		System.exit(1);
+    	}
+
     }
 
-    // inner helper class that is instanciated in above server loop and processes single job requests
+    /**
+     * SATELLITE THREAD
+     * -----------------------------------------------------------------------------------------
+     *  Inner Helper Method Class: instanciated in above server loop and processes single job requests
+     *  	--> take on Jobs, 
+     *  	--> get the right tool to do the job, 
+     *  	--> process the job & get the results back to whoever requested the job to be done.
+     */
     private class SatelliteThread extends Thread {
 
         Satellite satellite = null;
@@ -163,23 +204,73 @@ public class Satellite extends Thread {
         ObjectOutputStream writeToNet = null;
         Message message = null;
 
+        /**
+         * SatelliteThread () 
+         * -----------------------------------------------------------------------------------------
+	         * @param jobRequest
+	         * @param satellite
+         */
         SatelliteThread(Socket jobRequest, Satellite satellite) {
             this.jobRequest = jobRequest;
             this.satellite = satellite;
         }
 
+        /**
+         * run () 
+         * -----------------------------------------------------------------------------------------
+         */
         @Override
         public void run() {
-            // setting up object streams
-            // ...
+        	String toolType; // name of tool
+        	Tool tool; 
+        	
+            // SETTING UP OBJECT STREAMS
+            try {
+            	readFromNet = new ObjectInputStream(jobRequest.getInputStream()); 
+            	writeToNet  = new ObjectOutputStream(jobRequest.getOutputStream()); 
+        	
+            } catch (Exception e) {
+        		System.err.println("[SatelliteThread.run] Unable to connect....." + e); 
+        		System.exit(1);
+        	}
             
-            // reading message
-            // ...
-            
+        	
+            // READING MESSAGE.
+        	try {
+        		// cast as a message
+        		message = (Message) readFromNet.readObject (); 
+        		
+        	} catch (Exception e) {
+        		System.err.println("[SatelliteThread.run] Unable to read message....." + e); 
+        		System.exit(1);
+        	}
+
+            if (message == null) {
+        		System.err.println("[SatelliteThread.run] message is equal to null."); 
+        		return; 
+            }
+
+        	// DETERMINING WHICH TOOL TO USE.
             switch (message.getType()) {
                 case JOB_REQUEST:
-                    // processing job request
-                    // ...
+                	System.out.println("[SatelliteThread.run.JOB_REQUEST]");
+                	// processing job request
+                	Job job = (Job) message.getContent(); //create job
+                	
+                	// gets the right tool for the job
+                	toolType = job.getToolName(); 
+                	
+                	System.out.println("[SatelliteThread.run] Tool: " + toolType); 
+                	
+                	// Tell client the results //TODO
+                	try {
+                		tool = getToolObject (toolType); 
+                	} catch (IllegalAccessException | ClassNotFoundException 
+                				| InstantiationException | UnknownToolException e) {
+                		System.err.println("[SatelliteThread.run] Problem occured during "
+                								+ "getToolObject()....." + e); 
+                		System.exit(1);
+                	}
                     break;
 
                 default:
@@ -189,22 +280,53 @@ public class Satellite extends Thread {
     }
 
     /**
-     * Aux method to get a tool object, given the fully qualified class string
-     * If the tool has been used before, it is returned immediately out of the cache,
-     * otherwise it is loaded dynamically
+     * getToolObject ()
+     * -----------------------------------------------------------------------------------------
+     * Aux method to get the appropriate tool object, given the class string. If the tool has been 
+     * used before, it is returned immediately out of the cache, otherwise it is loaded dynamically.
      */
     public Tool getToolObject(String toolClassString) throws UnknownToolException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
         Tool toolObject = null;
-
-        // ...
+        toolObject = toolsCache.get(toolClassString);
         
+        // check if tool is in our cache as of right now
+        if (toolObject != null) {
+        	System.out.println("[SatelliteThread.getToolObject] Tool " 
+        						+ toolClassString + " is already in the cache.");
+        	
+        }
+        // have to add/load tool into cache 
+        else {
+        	System.out.println("[SatelliteThread.getToolObject] Tool " 
+					+ toolClassString + " is being added to the cache...");
+        	
+        	// load from web server (connected to classLoader) 
+        	Class toolClass = classLoader.loadClass(toolClassString); 
+        	toolObject = (Tool)toolClass.newInstance(); 
+        	toolsCache.put(toolClassString,  toolObject); 
+        	
+        }
         return toolObject;
     }
 
+    /**
+     * main
+     * -----------------------------------------------------------------------------------------
+     */
     public static void main(String[] args) {
-        // start the satellite
-        Satellite satellite = new Satellite(args[0], args[1], args[2]);
-        satellite.run();
+    	
+    	if (args.length != 3) {
+    		// need to make arguments for: satellite, classLoader, & server properties!
+    		Satellite satelliteImprov = new Satellite(
+    							"../../config/Satellite.Venus.properties", 
+    							"../../config/WebServer.properties", 
+    							"../../config/Server.properties");
+    		satelliteImprov.run(); 
+    	}
+    	else { // 3 arguments 
+    		Satellite satellite = new Satellite(args[0], args[1], args[2]);
+    		satellite.run();
+    	}
     }
 }
